c49d35a469258e5ee7af6458d4d037fa0e4a80f1
Benjamin <benjamin.alazet@gmail.com>
2023-07-06 13:19:12 +0200

    Modify tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,38 +1,55 @@
 #!/usr/bin/python3
+""" Module for BaseModel unit tests """
 import unittest
-import uuid
-from datetime import datetime
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
+import models
+from datetime import datetime
+import os
+import uuid
+import json
 
 
 class TestBaseModel(unittest.TestCase):
+    """Class test BaseModel"""
+    def test_id(self):
+        """ test of id attribute """
+        self.base = BaseModel()
+        self.base.id = str(uuid.uuid4())
+        self.assertEqual(str, type(self.base.id))
 
-    def setUp(self):
-        self.my_model = BaseModel()
-
-    def test_init(self):
-        self.assertTrue(isinstance(self.my_model, BaseModel))
-        self.assertIsInstance(self.my_model.created_at, datetime)
-        self.assertIsInstance(self.my_model.updated_at, datetime)
-        self.assertIsInstance(self.my_model.id, str)
-
-    def test_str(self):
-        model_str = "[{}] ({}) {}".format(self.my_model.__class__.__name__,
-                                          self.my_model.id, 
-                                          self.my_model.__dict__)
-        self.assertEqual(str(self.my_model), model_str)
-
-    def test_save(self):
-        old_updated_at = self.my_model.updated_at
-        self.my_model.save()
-        self.assertNotEqual(old_updated_at, self.my_model.updated_at)
+    def test_created_at(self):
+        """ test of created.at attribute """
+        self.base = BaseModel()
+        self.base.created_at = datetime.now()
+        self.assertIsNotNone(self.base.created_at)
 
     def test_to_dict(self):
-        model_dict = self.my_model.to_dict()
-        self.assertEqual(model_dict["created_at"], self.my_model.created_at.isoformat())
-        self.assertEqual(model_dict["updated_at"], self.my_model.updated_at.isoformat())
-        self.assertEqual(model_dict["__class__"], self.my_model.__class__.__name__)
-        self.assertEqual(model_dict["id"], self.my_model.id)
+        """ test of to_dict method """
+        self.base = BaseModel()
+        dic = self.base.to_dict()
+        self.assertIsInstance(dic['created_at'], str)
 
-if __name__ == "__main__":
+    def test__str__(self):
+        """ test of __str__ """
+        self.assertEqual(str, type(BaseModel.__str__(self)))
+
+    def test_save_check_time(self):
+        """ Tests that save method updates the time """
+        self.base = BaseModel()
+        self.base.name = "My_Model"
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+
+    def test_save_check_file(self):
+        """ Tests that save method saves contents """
+        self.base = BaseModel()
+        self.base.name = 'My_Model'
+        self.base.save()
+        with open("file.json", "r", encoding='utf-8') as f:
+            read_data = f.read()
+            self.assertIn(self.base.name, read_data)
+
+if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
-import unittest
-import os
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+import pep8
+import unittest
+import json
+
+""" Module for FileStorage unit tests """
 
 class TestFileStorage(unittest.TestCase):
+    """ contains all unit tests """
 
-    def setUp(self):
-        self.storage = FileStorage()
-        self.my_model = BaseModel()
-        self.storage.new(self.my_model)
+    def test__file_path(self):
+        """ tests the presence of file.json name """
+        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
+
+    def test_objects(self):
+        """ tests that __objects is a dictionary """
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_reload(self):
+        """ test that an exception is raised """
+        with self.assertRaises(TypeError):
+            storage.reload(None)
 
     def test_all(self):
-        all_objects = self.storage.all()
-        key = "{}.{}".format(type(self.my_model).__name__, self.my_model.id)
-        self.assertIn(key, all_objects.keys())
+        """ tests 'all' method """
+        self.assertEqual(FileStorage._FileStorage__objects, storage.all())
 
-    def test_new(self):
-        key = "{}.{}".format(type(self.my_model).__name__, self.my_model.id)
-        self.assertIn(key, self.storage.all().keys())
+    def test_save(self):
+        """ tests the 'save' method """
+        with open(FileStorage._FileStorage__file_path, 'r') as f:
+            self.assertEqual(dict, type(json.load(f)))
 
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
