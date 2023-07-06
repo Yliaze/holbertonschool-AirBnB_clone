e8c080d34b3cb841c8c6da71876888167cd47921
SimonDesc <pro.descombes@gmail.com>
2023-07-06 15:01:41 +0200

    create function do_create

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +20,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Print an empty line\n"""
         pass
+
+    def do_create(self, args=None):
+        list_arg = args.split()
+        if not list_arg:
+            print("** class name missing **")
+        else:
+            print(list_arg[0])
+            try:
+                obj = globals()[list_arg[0]]()
+                obj.save()
+                print(obj.id)
+            except:
+                print("** class doesn't exist **")
 
 
 if __name__ == "__main__":
